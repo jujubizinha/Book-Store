c294dcbf1f9771f7e64ace92862a988a7d4779f3
Júlia Silvestre <juliasilvestreribeiro2004@gmail.com>
2023-07-28 18:35:55 -0300

    fix: home section

--- script.js
+++ script.js
@@ -28,3 +28,22 @@ window.onload = () => {
     document.querySelector(".header .header-2").classList.remove("active");
   }
 };
+
+var swiper = new Swiper(".books-slider", {
+  loop: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
