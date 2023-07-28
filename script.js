73eabb33e670b6e72795c30b3b3db9572978281a
Júlia Silvestre <juliasilvestreribeiro2004@gmail.com>
2023-07-28 20:08:06 -0300

    feat: create featured section

--- script.js
+++ script.js
@@ -47,3 +47,27 @@ var swiper = new Swiper(".books-slider", {
     },
   },
 });
+
+var swiper = new Swiper(".featured-slider", {
+  loop: true,
+  centeredSlide: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    450: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+  },
+});
