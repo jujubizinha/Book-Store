55683e4622e408ddfd1c65eaf0e90084a29f87b2
Júlia Silvestre <juliasilvestreribeiro2004@gmail.com>
2023-07-30 18:59:14 -0300

    feat: create arrivals and deal section

--- script.js
+++ script.js
@@ -71,3 +71,25 @@ var swiper = new Swiper(".featured-slider", {
     },
   },
 });
+
+var swiper = new Swiper(".arrivals-slider", {
+  spaceBetween: 10,
+  loop: true,
+  centeredSlide: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
